faecc1ed1d608fc2b714040d6aa86cc09c222c3a
Sebastiaan Lokhorst <sebastiaanlokhorst@gmail.com>
2016-04-23 19:13:25 +0200

    Clean up everything

--- quantum1d.py
+++ quantum1d.py
@@ -3,44 +3,72 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as ani
 from numba import jit
 
-Lx = 2
-Nx = 1024
-dt = 1
-X = np.linspace(0, Lx, num=Nx)
-mom = np.linspace(-np.pi/Lx, +np.pi/Lx, num=Nx)
-Vwall = 100000
+class Quantum1D:
+	def __init__(self, Lx, Nx, dt):
+		self.t = 0
+		self.dt = dt
+		self.X = np.linspace(0, Lx, num=Nx)
+		self.mom = np.linspace(-np.pi/Lx, +np.pi/Lx, num=Nx)
+		self.V = np.ndarray([Nx], dtype=float)
+		self.V[:] = 0
+		self.psi = np.ndarray([Nx], dtype=complex)
+		self.psi[:] = 0
+		self.step_pos = np.exp(-1j*self.dt*self.V)
+		self.step_mom = np.exp(-1j*self.dt*self.mom**2/2)
 
-def update(psi0, V, dt):
-	A = np.exp(-1j*dt*V)
-	B = np.exp(-1j*dt*(mom)**2/2)
-	psi1 = np.fft.ifft(B*np.fft.fft(A*psi0))
-	return psi1
+	def set_V(self, V):
+		self.V[:] = V
+		self.step_pos = np.exp(-1j*self.dt*self.V)
 
-def wavepacket(x, sigma0, p0):
-	return np.power(np.sqrt(2*np.pi)*sigma0,-0.5) * np.exp(-x**2/(4*sigma0**2) + 1j*p0*x)
+	def set_psi(self, psi):
+		self.psi[:] = psi
 
-# IC:
-V = 0*X
-V[X<0.05*Lx] = Vwall
-V[X>0.95*Lx] = Vwall
-V += -1*(np.absolute(X-1)<0.01)
-psi = np.ndarray([Nx], dtype=complex)
-psi = wavepacket(X-0.2*Lx, 0.1, -5)
+	@jit
+	def update(self):
+		self.psi[:] = np.fft.ifft(self.step_mom*np.fft.fft(self.step_pos*self.psi))
+		self.t += self.dt
 
-# plot:
-fig = plt.figure()
-ax = plt.axes(xlim=(0, Lx), ylim=(-2, 10))
-psi_line, = ax.plot([], [], lw=2)
+	def plot(self):
+		self.fig = plt.figure()
+		self.ax = plt.axes(xlim=(self.X[0], self.X[-1]), ylim=(-2, 10))
+		self.ax.set_title('$|\Psi|^2$')
+		self.psi_line, = self.ax.plot([], [], lw=2)
+		plt.plot(self.X,self.V)
+		anim = ani.FuncAnimation(self.fig, self._ani_update, init_func=self._ani_init, interval=10, blit=True)
+		plt.show()
 
-def init():
-    psi_line.set_data([], [])
-    return psi_line,
+	def _ani_init(self):
+		self.psi_line.set_data([], [])
+		return self.psi_line,
 
-def animate(i):
-	psi[:] = update(psi, V, dt)
-	psi_line.set_data(X,np.square(np.absolute(psi)))
-	return psi_line,
+	def _ani_update(self,i):
+		self.update()
+		self.psi_line.set_data(self.X,np.square(np.absolute(self.psi)))
+		return self.psi_line,
 
-plt.plot(X,V)
-anim = ani.FuncAnimation(fig, animate, init_func=init, interval=10, blit=True)
-plt.show()
+if __name__ == "__main__":
+	# helper function...
+	def psi_wavepacket(x, sigma0, p0):
+		return np.power(np.sqrt(2*np.pi)*sigma0,-0.5) * np.exp(-x**2/(4*sigma0**2) + 1j*p0*x)
+
+	Lx = 2
+	Nx = 1024
+	dt = 1
+	q1d = Quantum1D(Lx, Nx, dt)
+
+	# parameter for the geometry
+	Vwall = 100000
+
+	# potential consists of walls to enclose the wave function ...
+	V = 0*q1d.X
+	V[q1d.X<0.05*Lx] = Vwall
+	V[q1d.X>0.95*Lx] = Vwall
+	# ... and a small well in the center
+	V += -0.1*(np.absolute(q1d.X-1)<0.02)
+	q1d.set_V(V)
+
+	# initial wave function is a wave packet with some momentum
+	psi = psi_wavepacket(q1d.X-0.2*Lx, 0.1, -10)
+	q1d.set_psi(psi)
+
+	q1d.plot()
--- quantum2d.py
+++ quantum2d.py
@@ -3,78 +3,111 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as ani
 from numba import jit
 
-Lx = 2
-Ly = 2
-Nx = 256
-Ny = 256
-dt = 1
-X = np.linspace(0, Lx, num=Nx)
-Y = np.linspace(0, Ly, num=Ny)
-momx = np.linspace(-np.pi/Lx, +np.pi/Lx, num=Nx)
-momy = np.linspace(-np.pi/Ly, +np.pi/Ly, num=Ny)
+class Quantum2D:
+	def __init__(self, Lx, Ly, Nx, Ny, dt, bc_func=None):
+		self.t = 0
+		self.dt = dt
+		self.bc_func = bc_func
+		self.X = np.linspace(0, Lx, num=Nx)
+		self.Y = np.linspace(0, Ly, num=Ny)
+		self.momx = np.linspace(-np.pi/Lx, +np.pi/Lx, num=Nx)
+		self.momy = np.linspace(-np.pi/Ly, +np.pi/Ly, num=Ny)
+		self.XX, self.YY = np.meshgrid(self.X,self.Y)
+		self.MX, self.MY = np.meshgrid(self.momx,self.momy)
+		self.V = np.ndarray([Nx,Ny], dtype=float)
+		self.V[:,:] = 0
+		self.psi = np.ndarray([Nx,Ny], dtype=complex)
+		self.psi[:,:] = 0
+		self.step_pos = np.exp(-1j*self.dt*self.V)
+		self.step_mom = np.exp(-1j*dt*(self.MX**2+self.MY**2)/2)
 
-XX, YY = np.meshgrid(X,Y)
-MX, MY = np.meshgrid(momx,momy)
+	def set_V(self, V):
+		self.V[:,:] = V
+		self.step_pos = np.exp(-1j*self.dt*self.V)
 
-Vwall = 100000
+	def set_psi(self, psi):
+		self.psi[:,:] = psi
 
-wallX = 0.5*Lx
-wallT = 0.01
-slitY1 = 0.45*Ly
-slitY2 = 0.55*Ly
-slitT = 0.04
+	@jit
+	def update(self):
+		if self.bc_func: # if a boundary condition is specified, enforce it now
+			self.bc_func(self)
+		self.psi[:,:] = np.fft.ifft2(self.step_mom*np.fft.fft2(self.step_pos*self.psi))
+		self.t += self.dt
 
-# make sure the wavefunction doesn't escape the box
-def V_container(x, y, Vwall, Lx, Ly):
-	return Vwall*(x<0.01*Lx or x>0.99*Lx or y<0.01*Ly or y>0.99*Ly)
-V_container = np.vectorize(V_container)
+	def psi_wavepacket(self, x, sigma0, p0):
+		return np.power(np.sqrt(2*np.pi)*sigma0,-0.5) * np.exp(-x**2/(4*sigma0**2) + 1j*p0*x)
 
-# wall with two slits
-def V_slit(x, y, Vwall, wallX, wallT, slitY1, slitY2, slitT):
-	return Vwall*((x > wallX-wallT and x < wallX+wallT) and ((y < slitY1-slitT/2 or y > slitY1+slitT/2) and (y < slitY2-slitT/2 or y > slitY2+slitT/2)))
-V_slit = np.vectorize(V_slit)
+	def plot(self):
+		self.fig = plt.figure()
+		self.ax1 = self.fig.add_subplot(121)
+		self.ax1.set_title('$|\Psi|^2$')
+		self.im1 = self.ax1.imshow(np.square(np.absolute(self.psi)))
+		self.ax2 = self.fig.add_subplot(122)
+		self.ax2.set_title('$Re(\Psi)^2$')
+		self.im2 = self.ax2.imshow(np.square(np.real(self.psi)))
+		anim = ani.FuncAnimation(self.fig, self._ani_update, init_func=self._ani_init)
+		plt.show()
 
-def V_geometry(x, y):
-	return V_container(XX, YY, Vwall, Lx, Ly) + V_slit(XX, YY, Vwall, wallX, wallT, slitY1, slitY2, slitT)
+	def _ani_init(self):
+		return
 
-def psi_wavepacket(x, y, sigma0, p0x, p0y):
-	return np.power(np.sqrt(2*np.pi)*sigma0,-0.5) * np.exp(-(x**2+y**2)/(4*sigma0**2) + 1j*(p0x*x+p0y*y))
+	def _ani_update(self,i):
+		self.update()
+		self.im1.set_array(np.square(np.absolute(self.psi))+V)
+		self.im2.set_array(np.square(np.real(self.psi))+V)
+		self.fig.canvas.draw()
+		return
 
-def psi_plane_wave(x, y, kx, ky, w, t):
-	return np.exp(1j*(x*kx+y*ky - w*t))
+if __name__ == "__main__":
+	# helper functions...
+	# box to enclose the domain so the wavefunction doesn't escape
+	def V_container(x, y, Vwall, Lx, Ly):
+		return Vwall*(x<0.01*Lx or x>0.99*Lx or y<0.01*Ly or y>0.99*Ly)
+	V_container = np.vectorize(V_container)
 
-@jit
-def update(psi0, V, dt):
-	A = np.exp(-1j*dt*V)
-	B = np.exp(-1j*dt*(MX**2+MY**2)/2)
+	# wall with two slits
+	def V_slit(x, y, Vwall, wallX, wallT, slitY1, slitY2, slitT):
+		return Vwall*((x > wallX-wallT and x < wallX+wallT) and ((y < slitY1-slitT/2 or y > slitY1+slitT/2) and (y < slitY2-slitT/2 or y > slitY2+slitT/2)))
+	V_slit = np.vectorize(V_slit)
 
-	psi1 = np.fft.ifft2(B*np.fft.fft2(A*psi0))
-	return psi1
+	# initial wave function for a wave packet
+	def psi_wavepacket(x, y, sigma0, p0x, p0y):
+		return np.power(np.sqrt(2*np.pi)*sigma0,-0.5) * np.exp(-(x**2+y**2)/(4*sigma0**2) + 1j*(p0x*x+p0y*y))
 
-V = V_geometry(XX, YY)
+	# wave function for a plane wave, useful for double-slit experiment
+	def psi_plane_wave(x, y, kx, ky, w, t):
+		return np.exp(1j*(x*kx+y*ky - w*t))
 
-psi = np.ndarray([Nx,Ny], dtype=complex)
-psi[:,Y<Ly/2-wallT/2] = psi_plane_wave(XX,YY,10,0,1,0)[:,Y<Ly/2-wallT/2]
-#psi[:] = psi_wavepacket(XX-0.3*Lx, YY-0.5*Ly, 0.1*Lx, 10, 0)
+	Lx = 2
+	Ly = 2
+	Nx = 256
+	Ny = 256
+	dt = 1
 
-fig = plt.figure()
-ax1 = fig.add_subplot(121)
-ax1.set_title('$|\Psi|^2$')
-im1 = ax1.imshow(np.square(np.absolute(psi)))
-ax2 = fig.add_subplot(122)
-ax2.set_title('$Re(\Psi)^2$')
-im2 = ax2.imshow(np.square(np.real(psi)))
-t=0
+	# parameters for the geometry
+	Vwall = 100000
+	wallX = 0.5*Lx
+	wallT = 0.01
+	slitY1 = 0.45*Ly
+	slitY2 = 0.55*Ly
+	slitT = 0.04
 
-def animate(i):
-	global t
-	t += dt
-	#force a plane wave in the left side
-	psi[:,Y<Ly/2-wallT/2] = psi_plane_wave(XX,YY,10,0,1,t)[:,Y<Ly/2-wallT/2]
-	psi[:] = update(psi, V, dt)
-	im1.set_array(np.square(np.absolute(psi))+V)
-	im2.set_array(np.square(np.real(psi))+V)
-	fig.canvas.draw()
+	# pass a boundary condition that will be enforced after every time step
+	def boundary_condition(q2d: Quantum2D):
+		q2d.psi[:,q2d.Y<Ly/2-wallT/2] = psi_plane_wave(q2d.XX,q2d.YY,10,0,1,q2d.t)[:,q2d.Y<Ly/2-wallT/2]
 
-anim = ani.FuncAnimation(fig, animate, interval=10, blit=False)
-plt.show()
+	q2d = Quantum2D(Lx, Ly, Nx, Ny, dt, bc_func=boundary_condition)
+
+	# potential consists of an enclosing box and a centered wall with a double slit
+	V = V_container(q2d.XX, q2d.YY, Vwall, Lx, Ly) + V_slit(q2d.XX, q2d.YY, Vwall, wallX, wallT, slitY1, slitY2, slitT)
+	q2d.set_V(V)
+
+	# psi is a plane wave on the left side
+	psi = np.ndarray([Nx,Ny], dtype=complex)
+	psi[:,:] = 0
+	psi[:,q2d.Y<Ly/2-wallT/2] = psi_plane_wave(q2d.XX,q2d.YY,10,0,1,0)[:,q2d.Y<Ly/2-wallT/2]
+	#psi[:] = psi_wavepacket(XX-0.3*Lx, YY-0.5*Ly, 0.1*Lx, 10, 0)
+	q2d.set_psi(psi)
+
+	q2d.plot()
