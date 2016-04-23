58375a88e504a42b4132a634d622ebdeef515d9a
Sebastiaan Lokhorst <sebastiaanlokhorst@gmail.com>
2016-04-23 16:32:49 +0200

    Add plane wave to quantum2d, clean up quantum1d

--- quantum2d.py
+++ quantum2d.py
@@ -3,13 +3,12 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import pyplot as plt
 from matplotlib import animation
-import time
+from numba import jit
 
 Lx = 2
 Ly = 2
-Nx = 128
-Ny = 128
-Nt = 1000
+Nx = 256
+Ny = 256
 dt = 1
 X = np.linspace(0, Lx, num=Nx)
 Y = np.linspace(0, Ly, num=Ny)
@@ -19,15 +18,34 @@ momy = np.linspace(-np.pi/Ly, +np.pi/Ly, num=Ny)
 XX, YY = np.meshgrid(X,Y)
 MX, MY = np.meshgrid(momx,momy)
 
-T = np.arange(0, dt*Nt, step=dt, dtype=float)
+Vwall = 100000
 
-def potential(x,y):
-	if (x > 0.79*Lx and x < 0.81*Lx) and ((y < 0.55*Ly or y > 0.56*Ly) and (y < 0.44*Ly or y > 0.45*Ly)):
-		return 1
-	else:
-		return 0
-potential = np.vectorize(potential)
+wallX = 0.5*Lx
+wallT = 0.01
+slitY1 = 0.45*Ly
+slitY2 = 0.55*Ly
+slitT = 0.04
 
+# make sure the wavefunction doesn't escape the box
+def V_container(x, y, Vwall, Lx, Ly):
+	return Vwall*(x<0.01*Lx or x>0.99*Lx or y<0.01*Ly or y>0.99*Ly)
+V_container = np.vectorize(V_container)
+
+# wall with two slits
+def V_slit(x, y, Vwall, wallX, wallT, slitY1, slitY2, slitT):
+	return Vwall*((x > wallX-wallT and x < wallX+wallT) and ((y < slitY1-slitT/2 or y > slitY1+slitT/2) and (y < slitY2-slitT/2 or y > slitY2+slitT/2)))
+V_slit = np.vectorize(V_slit)
+
+def V_geometry(x, y):
+	return V_container(XX, YY, Vwall, Lx, Ly) + V_slit(XX, YY, Vwall, wallX, wallT, slitY1, slitY2, slitT)
+
+def psi_wavepacket(x, y, sigma0, p0x, p0y):
+	return np.power(np.sqrt(2*np.pi)*sigma0,-0.5) * np.exp(-(x**2+y**2)/(4*sigma0**2) + 1j*(p0x*x+p0y*y))
+
+def psi_plane_wave(x, y, kx, ky, w, t):
+	return np.exp(1j*(x*kx+y*ky - w*t))
+
+@jit
 def update(psi0, V, dt):
 	A = np.exp(-1j*dt*V)
 	B = np.exp(-1j*dt*(MX**2+MY**2)/2)
@@ -35,46 +53,30 @@ def update(psi0, V, dt):
 	psi1 = np.fft.ifft2(B*np.fft.fft2(A*psi0))
 	return psi1
 
-def wavepacket(x, y, sigma0, p0x, p0y):
-	return np.power(np.sqrt(2*np.pi)*sigma0,-0.5) * np.exp(-(x**2+y**2)/(4*sigma0**2) + 1j*(p0x*x+p0y*y))
+V = V_geometry(XX, YY)
 
-def plane_wave(x, y, sigma0, p0x, p0y):
-	return np.power(np.sqrt(2*np.pi)*sigma0,-0.5) * np.exp(-(x**2)/(4*sigma0**2) + 1j*(p0x*x))
-
-def plane_wave1(x,p0x):
-	return np.exp(x+1j*(p0x*x))
-
-V = potential(XX,YY)
-#V += -1*(np.absolute(X-1)<0.005)
-#V = 0*X#1.5*(np.sign(X-0.7)+1-(np.sign(X-0.71)+1))*0
-psi = np.ndarray([Nt,Nx,Ny], dtype=complex)
-#psi[0] = wavepacket(XX-0.5*Lx, YY-0.5*Ly, 0.1, -50, 0)
-#psi[0] = plane_wave(XX-0.2*Lx, YY, 0.01, -50, 0)
-psi[0] = plane_wave1(XX-0.2*Lx,-50)
-for i in range(1,Nt):
-	psi[i] = update(psi[i-1], V, dt)
+psi = np.ndarray([Nx,Ny], dtype=complex)
+psi[:,Y<Ly/2-wallT/2] = psi_plane_wave(XX,YY,10,0,1,0)[:,Y<Ly/2-wallT/2]
+#psi[:] = psi_wavepacket(XX-0.3*Lx, YY-0.5*Ly, 0.1*Lx, 10, 0)
 
 fig = plt.figure()
-ax = fig.add_subplot(111)
-im = ax.imshow(np.square(np.absolute(psi[i])))
-plt.show(block=False)
-def init():
-    return
+ax1 = fig.add_subplot(121)
+ax1.set_title('$|\Psi|^2$')
+im1 = ax1.imshow(np.square(np.absolute(psi)))
+ax2 = fig.add_subplot(122)
+ax2.set_title('$Re(\Psi)^2$')
+im2 = ax2.imshow(np.square(np.real(psi)))
+t=0
 
 def animate(i):
-	Z = np.square(np.absolute(psi[i]))
-	Z += V
-	# wait for a second
-	#time.sleep(1)
-	# replace the image contents
-	im.set_array(Z)
-	# redraw the figure
+	global t
+	t += dt
+	#force a plane wave in the left side
+	psi[:,Y<Ly/2-wallT/2] = psi_plane_wave(XX,YY,10,0,1,t)[:,Y<Ly/2-wallT/2]
+	psi[:] = update(psi, V, dt)
+	im1.set_array(np.square(np.absolute(psi))+V)
+	im2.set_array(np.square(np.real(psi))+V)
 	fig.canvas.draw()
 
-# call the animator.  blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(fig, animate, init_func=init, frames=Nt, interval=1, blit=False)
-
-#j = np.array(range(1,50,10))
-#plt.plot(X,V,X,np.transpose(np.square(np.absolute(psi[j]))))
-#plt.plot(X,V)
+anim = animation.FuncAnimation(fig, animate, interval=10, blit=False)
 plt.show()
